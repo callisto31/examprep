d594692a77aea0ec529754da8090fbe49f901260
atamahma <93840057+callisto31@users.noreply.github.com>
2024-02-01 20:54:12 +0100

    Update findprevprime.go

--- findprevprime.go
+++ findprevprime.go
@@ -1,33 +1,23 @@
 package main
-
-import "fmt"
-
-func IsPrime(n int) bool {
-    if n <= 1 {
+func main(){
+}
+func FindPrevPrime(nbr int) int {
+    if nbr < 2 {
+        return 0
+    }
+    if IsPrime(nbr) {
+        return nbr
+    }
+    return FindPrevPrime(nbr - 1)
+}
+func IsPrime(nb int) bool {
+    if nb <= 0 || nb == 1 {
         return false
     }
-    for i := 2; i*i <= n; i++ {
-        if n%i == 0 {
+    for i := 2; i <= nb/2; i++ {
+        if nb%i == 0 {
             return false
         }
     }
     return true
-}
-
-func FindPrevPrime(nb int) int {
-    if nb <= 2 {
-        return -1 // No prime number less than or equal to 1
-    }
-
-    n := nb
-    for n > 2 && !IsPrime(n) {
-        n--
-    }
-
-    return n
-}
-
-func main() {
-    fmt.Println(FindPrevPrime(5))
-    fmt.Println(FindPrevPrime(8))
 }

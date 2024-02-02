e9679fd7abc5cc0b31919e34cb3ee4e3e0c2a95c
atamahma <93840057+callisto31@users.noreply.github.com>
2024-02-02 09:23:53 +0100

    Update fromto.go

--- fromto.go
+++ fromto.go
@@ -4,20 +4,32 @@ import "fmt"
 
 func FromTo(from, to int) string {
 	if from < 0 || from > 99 || to < 0 || to > 99 {
-		return "Invalid\n"
+		return "Invalid$\n"
 	}
 
 	var result string
-	for i := from; i <= to; i++ {
-		if i < 10 {
-			result += "0" + fmt.Sprint(i) + ", "
-		} else {
-			result += fmt.Sprint(i) + ", "
+	if from <= to {
+		for i := from; i <= to; i++ {
+			if i < 10 {
+				result += "0" + fmt.Sprint(i) + ", "
+			} else {
+				result += fmt.Sprint(i) + ", "
+			}
+		}
+	} else {
+		for i := from; i >= to; i-- {
+			if i < 10 {
+				result += "0" + fmt.Sprint(i) + ", "
+			} else {
+				result += fmt.Sprint(i) + ", "
+			}
 		}
 	}
 
 	if len(result) > 2 {
-		result = result[:len(result)-2] + "\n"
+		result = result[:len(result)-2] + "$\n"
+	} else {
+		result += "$\n"
 	}
 
 	return result
